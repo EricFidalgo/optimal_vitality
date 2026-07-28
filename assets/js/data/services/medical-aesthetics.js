0eeb507d5370caf0ec7cab37bbde1aad738cefd5
Eric Fidalgo <ericfidalgo.cs@gmail.com>
2026-07-28 08:32:43 -0400

    making website prod ready, created archive folder

--- assets/js/data/services/medical-aesthetics.js
+++
@@ -1,3 +0,0 @@
-// medical-aesthetics.js - Deprecated/Removed (Merged into Sexual Wellness & Regenerative Medicine)
-window.clinicData = window.clinicData || {};
-window.clinicData.services = window.clinicData.services || [];
